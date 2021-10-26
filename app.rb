413cff7048c126e17f6369fae7082e8e3ab3a955
Graeme Stirling <graemejstirling@gmail.com>
2021-10-26 09:03:04 +0100

    Second test passing

--- app.rb
+++ app.rb
@@ -10,5 +10,15 @@ class BookmarkManager < Sinatra::Base
     'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+    bookmarks = [
+              "http://www.makersacademy.com",
+              "http://www.destroyallsoftware.com",
+              "http://www.google.com"
+             ]
+  
+    bookmarks.join
+  end
+
   run! if app_file == $0
 end
