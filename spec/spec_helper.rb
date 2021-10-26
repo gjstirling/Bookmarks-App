db22c9fcb804f83de078cedc62c90fce369705e2
Graeme Stirling <graemejstirling@gmail.com>
2021-10-26 10:08:53 +0100

    Automating db clearing for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+require_relative './clear_test_db'
 # Set the environment to "test"
 ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    clear_test_database
+  end
+end
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
