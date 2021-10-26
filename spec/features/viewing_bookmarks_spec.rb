ecea5b2193b376d95ef18c729b69e1b68e275afe
Graeme Stirling <graemejstirling@gmail.com>
2021-10-26 10:30:47 +0100

    Extracted model logic from controller

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,13 +7,12 @@ end
 
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    # Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.askjeeves.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
     
+    # Add the test data
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.askjeeves.com")
+    Bookmark.create(url: "http://www.google.com")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
