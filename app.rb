dbc9f5f7af04f181ac5aca62613bcae67a8a528b
Graeme Stirling <graemejstirling@gmail.com>
2021-11-24 12:10:41 +0000

    green test delete bookmark

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra/reloader'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override 
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -22,6 +25,12 @@ class BookmarkManager < Sinatra::Base
 
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [params[:id]])
     redirect '/bookmarks'
   end
 
